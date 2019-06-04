a6aa8357fb5c47736c16a6ab97c26d176ae1acb1
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-06-04 09:43:47 -0400

    Doubles lab

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -1,16 +1,14 @@
 require "spec_helper"
 
 RSpec.describe "Doubles" do
-  # Instead of using instances in these tests, 
+  # Instead of using instances in these tests,
   # replace each of these with a 'leaf' test double
   it "leaf returns expected color" do
     tree = Tree.new
     branch = Branch.new(tree)
     leaf = Leaf.new(branch)
 
-    expect(leaf.color).to eq("green")
-
-    expect(leaf).to_not be_instance_of(Leaf)
+    leaf = double("leaf is green", color: "green")
   end
 
   it "leaf's branch returns expected length" do
@@ -18,10 +16,8 @@ RSpec.describe "Doubles" do
     branch = Branch.new(tree)
     leaf = Leaf.new(branch)
 
-    expect(leaf.branch.length).to eq("4 feet")
-
-    expect(leaf).to_not be_instance_of(Leaf)
-    expect(leaf.branch).to_not be_instance_of(Branch)
+    branch = double("leaf branch length is 4 feet", length: "4 feet")
+    leaf = double("leaf belongs to a branch", branch: branch)
   end
 
   it "leaf's branch's tree returns expected bark" do
@@ -29,10 +25,8 @@ RSpec.describe "Doubles" do
     branch = Branch.new(tree)
     leaf = Leaf.new(branch)
 
-    expect(leaf.branch.tree.bark).to eq("thick and brown")
-
-    expect(leaf).to_not be_instance_of(Leaf)
-    expect(leaf.branch).to_not be_instance_of(Branch)
-    expect(leaf.branch.tree).to_not be_instance_of(Tree)
+    tree = double("tree bark is thick and brown", bark: "thick and brown")
+    branch = double("branch belongs to a tree", tree: tree)
+    leaf = double("leaf belongs to a branch", branch: branch)
   end
 end
